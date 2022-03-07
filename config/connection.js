5b77d7ac731899b14e2dda240cf62640c1eaa21e
Luke G <lukejgranered@gmail.com>
2022-03-07 10:15:06 -0600

    user model and routes added, password protected

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    dialext: 'mysql',
+    dialect: 'mysql',
     port: 3306
 });
 
